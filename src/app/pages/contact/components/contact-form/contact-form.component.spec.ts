fa23ad0ac94dcddaa56947cbcd3cc75c5c5f6723
hm21 <alex.frei@hotmail.ch>
2024-06-26 17:29:26 +0200

    fix(contact-form): correct unit tests and adjust autofill color for dark mode
    Fixed the unit tests for the ContactFormComponent to ensure they pass and properly test the component's functionality. Adjusted the autofill color in dark mode by modifying the relevant styles in _input.scss to ensure proper visibility and user experience.

--- src/app/pages/contact/components/contact-form/contact-form.component.spec.ts
+++ src/app/pages/contact/components/contact-form/contact-form.component.spec.ts
@@ -1,6 +1,9 @@
 import { HttpTestingController } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { CONTACT_MESSAGES } from 'src/app/configs/contact-options';
 import { environment } from 'src/environments/environment';
 import { SharedTestingModule } from 'src/test/shared-testing.module';
 import { ContactFormComponent } from './contact-form.component';
@@ -9,11 +12,12 @@ describe('ContactFormComponent', () => {
   let component: ContactFormComponent;
   let fixture: ComponentFixture<ContactFormComponent>;
   let httpMock: HttpTestingController;
+  let submitButton: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ContactFormComponent, ReactiveFormsModule, SharedTestingModule],
-      teardown: {destroyAfterEach: false} 
+      teardown: { destroyAfterEach: false },
     }).compileComponents();
   });
 
@@ -29,6 +33,7 @@ describe('ContactFormComponent', () => {
     component.form.setValue(formValue);
     httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
+    submitButton = fixture.debugElement.query(By.css('button[type="submit"]'));
   });
 
   afterEach(() => {
@@ -39,30 +44,95 @@ describe('ContactFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should send form data', () => {
-    const mockResponse = {
-      success: true,
-      message: 'Form submission successful',
-    };
-
-    component.sendForm();
-
-    const req = httpMock.expectOne(environment.endpoints.contactMessage);
-    expect(req.request.method).toBe('POST');
-    req.flush(mockResponse);
-
-    expect(component['_sending']).toBeFalse();
-    expect(component['_sended']).toBeTrue();
+  it('should have a form with 4 controls', () => {
+    expect(component.form.contains('firstname')).toBeTruthy();
+    expect(component.form.contains('lastname')).toBeTruthy();
+    expect(component.form.contains('msg')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
   });
 
-  it('should handle error responses', () => {
-    const mockErrorResponse = { status: 400, statusText: 'Bad Request' };
+  it('should mark form as invalid if fields are empty', () => {
+    component.form.setValue({
+      firstname: '',
+      lastname: '',
+      msg: '',
+      email: '',
+    });
+    expect(component.form.invalid).toBeTrue();
+  });
 
-    component.sendForm();
+  it('should mark email as invalid if email format is incorrect', () => {
+    component.form.controls.email.setValue('invalid-email');
+    expect(component.form.controls.email.invalid).toBeTrue();
+  });
+
+  it('should enable submit button when form is valid', () => {
+    component.form.setValue({
+      firstname: 'John',
+      lastname: 'Doe',
+      msg: 'Hello!',
+      email: 'john.doe@example.com',
+    });
+    fixture.detectChanges();
+    expect(submitButton.nativeElement.disabled).toBeFalse();
+  });
+
+  it('should update formState$ to error if form is invalid on submit', () => {
+    spyOn(component.formState$, 'next');
+    component.form.setValue({
+      firstname: '',
+      lastname: '',
+      msg: '',
+      email: '',
+    });
+    component.submit$.next();
+    expect(component.formState$.next).toHaveBeenCalledWith({
+      state: 'error',
+      msg: CONTACT_MESSAGES.invalidEmail,
+      canSend: true,
+    });
+  });
+
+  it('should send form data when form is valid and update formState$ to success', () => {
+    component.form.setValue({
+      firstname: 'John',
+      lastname: 'Doe',
+      msg: 'Hello!',
+      email: 'john.doe@example.com',
+    });
+    component.submit$.next();
 
     const req = httpMock.expectOne(environment.endpoints.contactMessage);
     expect(req.request.method).toBe('POST');
-    req.flush({}, mockErrorResponse);
-    expect(component['_sending']).toBeFalse();
+    req.flush({});
+
+    expect(component.formState$.value.state).toBe('success');
+    expect(component.formState$.value.msg).toBe(
+      CONTACT_MESSAGES.submissionSuccess,
+    );
+    expect(component.formState$.value.canSend).toBeFalse();
+  });
+
+  it('should handle error response correctly', () => {
+    spyOn(component.formState$, 'next');
+    component.form.setValue({
+      firstname: 'John',
+      lastname: 'Doe',
+      msg: 'Hello!',
+      email: 'john.doe@example.com',
+    });
+    component.submit$.next();
+
+    const req = httpMock.expectOne(environment.endpoints.contactMessage);
+    req.flush(
+      { error: 'Blacklist' },
+      { status: 400, statusText: 'Bad Request' },
+    );
+
+    expect(component.formState$.next).toHaveBeenCalledWith({
+      state: 'error',
+      msg: jasmine.any(String),
+      canSend: true,
+    });
   });
 });
