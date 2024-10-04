8dee4e26d3be5b82039512623e5ebe121cb64617
hm21 <alex.frei@hotmail.ch>
2024-10-04 21:00:44 +0200

    fix(modal): resolve issues in modal service unit tests

--- src/app/shared/modal/modal.service.spec.ts
+++ src/app/shared/modal/modal.service.spec.ts
@@ -1,6 +1,4 @@
-import { ViewContainerRef } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { viewContainerRefMock } from 'src/test/mocks/view-container-ref.mock';
 import { SharedTestingModule } from 'src/test/shared-testing.module';
 import { ModalService } from './modal.service';
 
@@ -10,13 +8,6 @@ describe('ModalManagerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [SharedTestingModule],
-      providers: [
-        {
-          provide: ViewContainerRef,
-          useValue: viewContainerRefMock,
-        },
-        ModalService,
-      ],
       teardown: { destroyAfterEach: false },
     });
     service = TestBed.inject(ModalService);
