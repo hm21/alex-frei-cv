14853f710bfd036f0522d912a164bd59dce8aff5
hm21 <alex.frei@hotmail.ch>
2024-10-30 15:48:06 +0100

    feat(git-commits): enhance read function to fetch full commit count by iterating over all pages

--- functions/src/git-commits.ts
+++ functions/src/git-commits.ts
@@ -28,26 +28,36 @@ export default async (req: Request, resp: express.Response) => {
     // Delete older temporary git commits to save storage
     await commitRtdbRef.remove().catch(error);
 
-    // Get current commit count from GitHub API
-    return await axios
-      .get('https://api.github.com/repos/hm21/alex-frei-cv/commits', {
-        params: {
-          // When reuse this code for large projects, is it smarter to wrap it in a while-loop
-          // and work with multiple pages to get the full size => Remember the read limits from github
-          per_page: 1_000,
-        },
-      })
-      .then(async (response) => {
-        const count = response.data.length ?? 0;
+    let count = 0;
+    let page = 1;
+    let shouldFetchMore = true;
+    const perPageLimit = 100;
 
-        // Set current commit count in the database
-        await commitRtdbRef.child(formatDate(dt)).set(count).catch(error);
+    while (shouldFetchMore) {
+      // Get current commit count from GitHub API
+      const response = await axios
+        .get('https://api.github.com/repos/hm21/alex-frei-cv/commits', {
+          params: {
+            per_page: perPageLimit,
+            page,
+          },
+        })
+        .catch(() => {
+          throw new HttpsError('unknown', 'Failed to get git commits');
+        });
+      const pageCommitCount = response.data?.length ?? 0;
+      count += pageCommitCount;
 
-        return resp.status(200).json(count);
-      })
-      .catch(() => {
-        throw new HttpsError('unknown', 'Failed to get git commits');
-      });
+      if (pageCommitCount === perPageLimit) {
+        page += 1;
+      } else {
+        shouldFetchMore = false;
+      }
+    }
+    // Set current commit count in the database
+    await commitRtdbRef.child(formatDate(dt)).set(count).catch(error);
+
+    return resp.status(200).json(count);
   }
 };
 function formatDate(date: Date): string {
