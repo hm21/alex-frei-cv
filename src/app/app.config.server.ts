f9e9d2bb056759cddd96bcf8f5d814937eda0f53
hm21 <alex.frei@hotmail.ch>
2025-10-03 16:31:56 +0200

    chore(dependencies): update Angular and ESLint packages to latest versions

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,5 +1,4 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
@@ -7,7 +6,6 @@ import { serverRoutes } from './app.routes.server';
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(withRoutes(serverRoutes)),
-    provideNoopAnimations(),
   ],
 };
 
